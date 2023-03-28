1eb027ada4991b6a8f8cf7428bbb2f7b334666d2
Ritvik5 <sharma.ritvik56@gmail.com>
2023-03-28 14:15:44 +0530

    Welcome Message

---
+++ AddressBookSystem/Contacts.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    class Contacts
+    {
+        public string FirstName;
+        public string LastName;
+        public string Address;
+        public string City;
+        public string State;
+        public int Zip;
+        public int PhoneNumber;
+        public string Email;
+        public Contacts(string firstName, string lastName, string addr, string city,string state, int zip, int phoneNumber, string email)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Address = addr;
+            City = city;
+            State = state;
+            Zip = zip;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+    }
+}
